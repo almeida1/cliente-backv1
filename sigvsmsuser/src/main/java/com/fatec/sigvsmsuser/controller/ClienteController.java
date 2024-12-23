227f7de4e4f60238fb473da7b4ffa301abf6045c
almeida1 <engsoftware1@gmail.com>
2024-12-23 12:51:18 -0300

    funcao - consulta todos

--- sigvsmsuser/src/main/java/com/fatec/sigvsmsuser/controller/ClienteController.java
+++ sigvsmsuser/src/main/java/com/fatec/sigvsmsuser/controller/ClienteController.java
@@ -1,10 +1,12 @@
 package com.fatec.sigvsmsuser.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +22,15 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("/api/v1/clientes")
 public class ClienteController {
-	
+
 	final ClienteService clienteService;
-	
-	
-	public ClienteController (ClienteService usuarioService) {
+
+	public ClienteController(ClienteService usuarioService) {
 		this.clienteService = usuarioService;
 	}
+
 	@PostMapping
-	public ResponseEntity<Object> saveCliente(@RequestBody @Valid ClienteRecordDTO cliente){
+	public ResponseEntity<Object> saveCliente(@RequestBody @Valid ClienteRecordDTO cliente) {
 		Cliente novoCliente = new Cliente();
 		novoCliente.setCpf(cliente.cpf());
 		novoCliente.setNome(cliente.nome());
@@ -40,7 +42,13 @@ public class ClienteController {
 		} else {
 			return ResponseEntity.status(HttpStatus.CREATED).body(c.get());
 		}
-		
+
+	}
+
+	@GetMapping("/all")
+	public List<Cliente> getAll() {
+		// logger.info(">>>>>> apicontroller consulta todos iniciado...");
+		return clienteService.consultaTodos();
 	}
 
 }
